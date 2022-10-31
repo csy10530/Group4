e16bfedc0e307f7cdffa7cf48c079fe1fffc8c08
Siyuan Chen <chen.siyuan@northeastern.edu>
2022-10-30 20:08:25 -0400

    added mui

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,92 +1,120 @@
-import React from 'react';
-import {Field, Form, Formik} from 'formik';
+import React, {useState} from 'react';
+import {useFormik} from 'formik';
+import * as yup from 'yup';
 import {useAppDispatch, useAppSelector} from "../../app/hooks";
 import {register, selectMockUsers} from "./signupSlice";
+
+import IconButton from "@mui/material/IconButton";
+import InputAdornment from "@mui/material/InputAdornment";
+import {Visibility, VisibilityOff} from "@mui/icons-material";
+import Button from "@mui/material/Button"
+import TextField from '@mui/material/TextField';
+
+const SignupSchema = yup.object().shape({
+    firstName: yup.string()
+        .min(2, "Too short")
+        .max(50, "Too Long")
+        .required("Required"),
+    lastName: yup.string()
+        .min(2, "Too short")
+        .max(50, "Too Long")
+        .required("Required"),
+    password: yup.string()
+        .min(5, "Too short")
+        .max(15, "Too long")
+        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{5,15})/,
+            "Must between 5 to 15 characters, one uppercase, one lowercase, one number and one special character")
+        .required("Required"),
+    email: yup.string().email("Invalid email").required("Required")
+});
 
 export const Signup: React.FC<{}> = () => {
     const mockUsers = useAppSelector(selectMockUsers);
     const dispatch = useAppDispatch();
 
-    const validateEmail = (val: string) => {
-        let err;
-        if (!val) {
-            err = "Please enter your email!";
-        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val)) {
-            err = "Invalid email address.";
-        }
-        return err;
-    }
+    const [showPassword, setShowPassword] = useState(false);
 
-    const validateFirstName = (val: string) => {
-        if (!val) {
-            return "Please enter your first name!"
-        }
-    }
-
-    const validateLastName = (val: string) => {
-        if (!val) {
-            return "Please enter your last name!"
-        }
-    }
-
-    const validatePassword = (val: string) => {
-        if (!val) {
-            return "Please enter your password!"
-        }
-    }
+    const formik = useFormik({
+        initialValues: {
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+        },
+        validationSchema: SignupSchema,
+        onSubmit: (values) => {
+            dispatch(register(values));
+        },
+    });
 
     return (
-        <React.Fragment>
-            <nav></nav>
-            <div>
-                <h1>Create account</h1>
-                <Formik
-                    initialValues={{
-                        firstName: "",
-                        lastName: "",
-                        password: "",
-                        confirmPassword: "",
-                        email: ""
+        <div>
+            <form onSubmit={formik.handleSubmit}>
+                <TextField
+                    id="firstName"
+                    name="firstName"
+                    label="First Name"
+                    value={formik.values.firstName}
+                    onChange={formik.handleChange}
+                    error={formik.touched.firstName && Boolean(formik.errors.firstName)}
+                    helperText={formik.touched.firstName && formik.errors.firstName}
+                />
+
+                <TextField
+                    id="lastName"
+                    name="lastName"
+                    label="Last Name"
+                    value={formik.values.lastName}
+                    onChange={formik.handleChange}
+                    error={formik.touched.lastName && Boolean(formik.errors.lastName)}
+                    helperText={formik.touched.lastName && formik.errors.lastName}
+                />
+
+                <TextField
+                    id="password"
+                    name="password"
+                    label="password"
+                    type={showPassword ? "text" : "password"}
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                    error={formik.touched.password && Boolean(formik.errors.password)}
+                    helperText={formik.touched.password && formik.errors.password}
+                    InputProps={{
+                        endAdornment: <InputAdornment position="end">
+                            <IconButton
+                                onClick={() => {
+                                    setShowPassword(!showPassword)
+                                }}
+                            >
+                                {showPassword ? <VisibilityOff/> : <Visibility/>}
+                            </IconButton>
+                        </InputAdornment>
                     }}
+                />
 
-                    onSubmit={values => {
-                        dispatch(register(values));
-                    }}>
+                <TextField
+                    id="email"
+                    name="email"
+                    label="Email"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    error={formik.touched.email && Boolean(formik.errors.email)}
+                    helperText={formik.touched.email && formik.errors.email}
+                />
 
-                    {({errors, touched, values, isValidating}) => (
-                        <Form>
-                            <label htmlFor="firstName">First Name</label>
-                            <Field id="firstName" name="firstName" type="text" validate={validateFirstName}/>
-                            {errors.firstName && touched.firstName && <div>{errors.firstName}</div>}
+                <Button color="primary" variant="contained" type="submit">
+                    Create Account
+                </Button>
 
-                            <label htmlFor="lastName">Last Name</label>
-                            <Field id="lastName" name="lastName" type="text" validate={validateLastName}/>
-                            {errors.lastName && touched.lastName && <div>{errors.lastName}</div>}
-
-                            <label htmlFor="password">Password</label>
-                            <Field id="password" name="password" type="password" validate={validatePassword}/>
-                            {errors.password && touched.password && <div>{errors.password}</div>}
-
-                            <label htmlFor="email">Email</label>
-                            <Field id="email" name="email" type="email" validate={validateEmail}/>
-                            {errors.email && touched.email && <div>{errors.email}</div>}
-
-                            <button type="submit">Create Account</button>
-
-                            {/*test*/}
-                            <div>{mockUsers.map(user => {
-                                return <div>
-                                    <span>{user.firstName} </span>
-                                    <span>{user.lastName} </span>
-                                    <span>{user.password} </span>
-                                    <span>{user.email} </span>
-                                </div>
-                            })}</div>
-                        </Form>
-                    )}
-                </Formik>
-
-            </div>
-        </React.Fragment>
-    )
-}
+                <div>{mockUsers.map(user => {
+                    return <div>
+                        <span>{user.firstName} </span>
+                        <span>{user.lastName} </span>
+                        <span>{user.password} </span>
+                        <span>{user.email} </span>
+                    </div>
+                })}</div>
+            </form>
+        </div>
+    );
+};
